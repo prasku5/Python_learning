05e24f42eb7277d65d9ec315208fe985aee22f07
prasku5 <prasnicks@gmail.com>
2022-03-24 12:59:13 -0400

    Variables

---
+++ Datacamp/Basics/variable.py
@@ -0,0 +1,16 @@
+# Variables in python are case sensitive
+# type gives the datatype of the value
+
+
+# Definition of savings and result
+savings = 100
+result = 100 * 1.10 ** 7
+
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
