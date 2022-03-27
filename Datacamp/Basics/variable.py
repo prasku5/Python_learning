3a056c4d0c264bb71cb9b3b1b6b1ff565b677eec
prasku5 <prasnicks@gmail.com>
2022-03-26 20:41:21 -0400

    file changes

--- Datacamp/Basics/variable.py
+++ Datacamp/Basics/variable.py
@@ -7,7 +7,8 @@ savings = 100
 result = 100 * 1.10 ** 7
 
 # Fix the printout
-print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
+print("I started with $" + str(savings)
+      + " and now have $" + str(result) + ". Awesome!")
 
 # Definition of pi_string
 pi_string = "3.1415926"
