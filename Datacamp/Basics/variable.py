cf4fa8b289a6a14ad27ff48e765f9bd56d1124c5
prasku5 <prasnicks@gmail.com>
2022-03-29 21:48:46 -0400

    variables update

--- Datacamp/Basics/variable.py
+++ Datacamp/Basics/variable.py
@@ -10,8 +10,28 @@ result = 100 * 1.10 ** 7
 print("I started with $" + str(savings)
       + " and now have $" + str(result) + ". Awesome!")
 
-# Definition of pi_string
+# Definition of pi_stri ng
 pi_string = "3.1415926"
 
 # Convert pi_string into float: pi_float
 pi_float = float(pi_string)
+
+# Variable assignment
+
+# Create a variable savings
+savings = 100
+
+# Print out savings
+print(savings)
+
+# Create a variable savings
+savings = 100
+
+# Create a variable growth_multiplier
+growth_multiplier = 1.1
+
+# Calculate result
+result = savings * growth_multiplier ** 7
+
+# Print out result
+print(result)
