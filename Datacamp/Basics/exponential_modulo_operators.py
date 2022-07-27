a014a2dc8cce4a550a1649765cdc9bf8eb898876
prasku5 <prasnicks@gmail.com>
2022-07-27 15:47:11 -0400

    added Modulo comment

--- Datacamp/Basics/exponential_modulo_operators.py
+++ Datacamp/Basics/exponential_modulo_operators.py
@@ -5,7 +5,7 @@ print(5 - 5)
 # Multiplication, division, modulo, and exponentiation
 print(3 * 5)
 print(10 / 2)
-print(18 % 7)
+print(18 % 7) #Modulo sign
 print(4 ** 2)
 
 # How much is your $100 worth after 7 years?
